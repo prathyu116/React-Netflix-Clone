bd913d105acf244bc8e8adca75c1b4896241409a
prathyu <prathyuprasad04669@gmail.com>
2021-06-02 01:04:59 +0530

    responsive

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import logo from './../../assets/netflix.png'
 import avatar from './../../assets/avatar.png'
 import './NavBar.css'
+import { useState ,useEffect} from 'react'
 const NavBar = () => {
+    const [state, setState] = useState()
+    useEffect(() => {
+        window.addEventListener('scroll',()=>{
+            if(window.scrollY>100){
+                setState(true)
+            }else{
+                setState(false)
+            }
+        });
+        return()=>{
+            window.removeEventListener('scroll')
+        }
+       
+    }, [])
     return (
-        <div className="nav-container">
+        <div className={`nav-container ${state && "nav-black"}`}>
             <img className="netflix" src={logo} alt="" />
             
             <img className="avatar" src={avatar} alt="" />
